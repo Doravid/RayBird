a433eecda1ab3f33501895c48e1045a5accab91b
Doravid <74063222+Doravid@users.noreply.github.com>
2025-04-30 18:59:59 -0400

    lol

--- src/maps/levelEditor.zig
+++ src/maps/levelEditor.zig
@@ -13,8 +13,8 @@ const spk = blockType.spk;
 const bdy = blockType.bdy;
 const frt = blockType.frt;
 const nul = blockType.null;
-
 pub var currentBlock: blockType = sol;
+var waitingOnInput = false;
 
 const level = struct {
     map: [9][16]blockType,
@@ -41,8 +41,13 @@ pub fn initLevelEditor() void {
 }
 var body = std.ArrayList(player.pos).init(std.heap.page_allocator);
 
+var userInput: [64:0]u8 = undefined;
+var view: bool = true;
+
 pub fn loadLevelEditor() void {
-    if (rl.isMouseButtonPressed(rl.MouseButton.left)) {
+    const text_box = rl.Rectangle{ .height = 1.25 * @as(f32, @floatFromInt(game.boxSize)), .width = 4.0 * @as(f32, @floatFromInt(game.boxSize)), .x = (@as(f32, @floatFromInt(game.screenWidth)) / 2.0) - 240.0, .y = 80 };
+
+    if (rl.isMouseButtonPressed(rl.MouseButton.left) and !waitingOnInput) {
         const pos = rl.getMousePosition();
         const replacedBlock = game.getBlockAt(@intFromFloat(pos.x), @intFromFloat(pos.y));
         if (currentBlock == nul and replacedBlock == bdy) {}
@@ -55,9 +60,20 @@ pub fn loadLevelEditor() void {
                 std.debug.print("Failed to append position: {}\n", .{err});
                 return;
             };
-            writeLevelToFile(level.init(player.mat16x9, body.items));
+            if (!waitingOnInput) {
+                waitingOnInput = true;
+                return;
+            }
 
             std.debug.print("POS: {}\n", .{x});
+        }
+    }
+    if (waitingOnInput) {
+        userInput[0] = 'H';
+        const res = gui.guiTextInputBox(text_box, "", "Please enter level name:", "Save;Cancel", &userInput, 64, &view);
+        if (res == 1) {
+            waitingOnInput = false;
+            writeLevelToFile(level.init(player.mat16x9, body.items), userInput);
         }
     }
     const numBlocks: comptime_int = 5;
@@ -70,20 +86,27 @@ pub fn loadLevelEditor() void {
     }
 }
 
-fn writeLevelToFile(level1: level) void {
+fn writeLevelToFile(level1: level, name: [64:0]u8) void {
     const allocator = std.heap.page_allocator;
-    var userInput: [64:0]u8 = undefined;
-    var view: bool = true;
-    const text_box = rl.Rectangle{ .height = 1.25 * @as(f32, @floatFromInt(game.boxSize)), .width = 4.0 * @as(f32, @floatFromInt(game.boxSize)), .x = (@as(f32, @floatFromInt(game.screenWidth)) / 2.0) - 240.0, .y = 80 };
-    while (true) {
-        _ = gui.guiTextInputBox(text_box, "User Input", "Please enter your username:", "OK;Cancel", &userInput, 64, &view);
-    }
+
+    const name_slice = std.mem.sliceTo(&name, 0);
+    const name_len = name_slice.len;
+    const prefix = "./src/maps/";
+    const suffix = ".json";
+    const path = allocator.alloc(u8, prefix.len + name_len + suffix.len) catch |err| {
+        std.debug.print("Failed to append position: {}\n", .{err});
+        return;
+    };
+    // std.mem.copyForwards(u8, path[0..], prefix);
+    // std.mem.copyForwards(u8, path[prefix.len..], name[0..name_len]);
+    // std.mem.copyForwards(u8, path[prefix.len + name_len ..], suffix);
+
     //Print to file
     const string = json.stringifyAlloc(allocator, level1, .{ .emit_strings_as_arrays = false }) catch |err| {
         std.debug.print("Failed to append position: {}\n", .{err});
         return;
     };
-    var file = fs.cwd().createFile("./src/maps/level1.json", .{}) catch |err| {
+    var file = fs.cwd().createFile(path, .{}) catch |err| {
         std.debug.print("Failed to append position: {}\n", .{err});
         return;
     };
